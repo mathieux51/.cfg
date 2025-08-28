5548654df372d7b7fdc246c50797a473f51089b1
Mathieu <mathieux51@gmail.com>
2025-08-28 10:23:12 +0200

    tidy setup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ config.font_size = 21.0
 
 config.window_decorations = "RESIZE"
 
-config.default_prog = tmux
+config.default_prog = { 'tmux' }
 config.hide_tab_bar_if_only_one_tab = true
 
 -- if tmux == true then
